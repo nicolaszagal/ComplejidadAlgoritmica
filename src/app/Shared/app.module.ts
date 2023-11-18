b05a0ce2e50e8d628de9f341df60d777607c98b4
DarkArcher822 <112427427+nicolaszagal@users.noreply.github.com>
2023-11-18 02:25:35 -0500

    algoritmo de dijkstra funcionando

--- src/app/Shared/app.module.ts
+++ src/app/Shared/app.module.ts
@@ -18,6 +18,7 @@ import {MatInputModule} from "@angular/material/input";
 import {MatSelectModule} from "@angular/material/select";
 import {MatOptionModule} from "@angular/material/core";
 import {MatAutocompleteModule} from "@angular/material/autocomplete";
+import {CardComponent} from "./Components/card/card.component";
 
 
 @NgModule({
@@ -26,7 +27,8 @@ import {MatAutocompleteModule} from "@angular/material/autocomplete";
     ToolbarComponent,
     InicioComponent,
     FooterComponent,
-    MyFlightsComponent
+    MyFlightsComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
