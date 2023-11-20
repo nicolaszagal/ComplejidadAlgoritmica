18bf8b6e59350e271603ae784098636f9f39a644
DarkArcher822 <112427427+nicolaszagal@users.noreply.github.com>
2023-11-19 22:39:12 -0500

    Archivos para el deploy

--- functions/api.js
+++ functions/api.js
@@ -1,9 +1,27 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('../src/assets/db.json');
-const middlewares = jsonServer.defaults();
+// api.js
+const express = require('express');
+const serverless = require('serverless-http');
+const app = express();
+const path = require('path');
 
-server.use(middlewares);
-server.use(router);
+app.use(express.json());
 
-module.exports = server;
+app.get('/usuarios', (req, res) => {
+  const dbPath = path.resolve(__dirname, '../src/assets/db.json');
+  res.sendFile(dbPath);
+});
+app.get('/destinos', (req, res) => {
+  const dbPath = path.resolve(__dirname, '../src/assets/db.json');
+  res.sendFile(dbPath);
+});
+app.get('/guardados', (req, res) => {
+  const dbPath = path.resolve(__dirname, '../src/assets/db.json');
+  res.sendFile(dbPath);
+});
+app.get('/vuelos', (req, res) => {
+  const dbPath = path.resolve(__dirname, '../src/assets/db.json');
+  res.sendFile(dbPath);
+});
+
+module.exports = app;
+module.exports.handler = serverless(app);
